e013c270d281c043cab5aeb424b58659bbc6fe81
unknown <prohlad.m99@gmail.com>
2022-04-08 22:25:01 +0600

    DemoReact

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import AllStudents from './MyComponents/AllStudents';
+
+function App() {
+  return (
+    <div className="App">
+      <AllStudents/>
+    </div>
+  );
+}
+
+export default App;
